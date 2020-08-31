be5fd0a1eb41d1315e26359a5bba07373bce1f3a
Federico Orazi <federico.orazi@studio.unibo.it>
2020-08-31 16:15:15 +0200

    assdsdsdsdsdsaasaer$Rf<srf

--- src/test/scala/test.scala
+++ src/test/scala/test.scala
@@ -1,10 +1,11 @@
+import org.scalatest._
+
 class SetSuite extends FunSuite {
 	test ("An empty Set should have size 0") {
 		assert(Set.empty.size == 0)
-	}	
+	}
 
-	test (" Calling head on an empty Set should yield
-		NoSuchElementException ") {
+	test ("Calling head on an empty Set should yield NoSuchElementException") {
 			assertThrows[NoSuchElementException] {
 			Set.empty.head
 		}
